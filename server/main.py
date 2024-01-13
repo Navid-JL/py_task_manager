a3ef77c22164b6b391be8d1c7ac0ce2bb152e5b8
Navid <navidjalili3@gmail.com>
2024-01-13 20:10:16 +0330

    Tie up some loose ends

--- server/main.py
+++ server/main.py
@@ -5,11 +5,13 @@ from api.models import product_model
 from api.database import engine
 from api.routers.product import router as product_router
 
-app = FastAPI(title="PyTaskManager")
+app = FastAPI(title="PyTaskManager", version="1.0.0",
+              summary="A very basic API as a result of a personal experiment with FastAPI",
+              contact={"name": "Navid Jalili", "url": "https://github.com/Navid-JL#-contact", "email": "navidjalili3@gmail.com"}, license_info={"name": "CC0 1.0 Universal"})
 
 product_model.Base.metadata.create_all(engine)
 
-app.include_router(product_router, prefix="/product")
+app.include_router(product_router, prefix="/product", tags=["Product"])
 
 logging.basicConfig(
     filename="./api/logs/app.log",
